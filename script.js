49b338ab6cbe81dbe5fb86afb5ea0854f3f2fe37
sonukushwaha10x-sketch <sonukushwaha10x@gmail.com>
2026-06-28 23:15:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-result.innerHTML = `
+import { db } from "./firebase-config.js";
+console.log("Firebase connected:", db);result.innerHTML = `
   <h2 style="color:green;">✅ Certificate Verified</h2>
   <p><b>Name:</b> ${data.name}</p>
   <p><b>Course:</b> ${data.course}</p>
