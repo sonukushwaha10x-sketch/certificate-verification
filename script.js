35af0448a656cfe04cfefce1775d4f0ebf77b6de
sonukushwaha10x-sketch <sonukushwaha10x@gmail.com>
2026-06-28 22:53:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+result.innerHTML = `
+  <h2 style="color:green;">✅ Certificate Verified</h2>
+  <p><b>Name:</b> ${data.name}</p>
+  <p><b>Course:</b> ${data.course}</p>
+  <p><b>Issue Year:</b> ${data.year}</p>
+  <p><b>Certificate No:</b> ${data.certificateNo}</p>
+  <p><b>Status:</b> ${data.status}</p>
+`;
